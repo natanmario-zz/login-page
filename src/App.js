63308bec8393aac0956ac384192c1895ea4834a7
natanmario <natanmario@cardapioweb.com>
2022-05-07 12:07:32 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Style } from "./styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Style>
+      <div className='background'>
+        <img src='Right Side.svg' alt='' />
+      </div>
+
+      <div className='form'>
+        <div className='header'>
+          <h1 className='title'>Welcome back</h1>
+
+          <h4 className='subtitle'>Welcome back! Please enter your details.</h4>
+        </div>
+
+        <div className='fields'>
+          <div className='field'>
+            <span className='label'>E-mail</span>
+            <input
+              className='input-h'
+              type='email'
+              placeholder='Enter your email'
+            />
+          </div>
+
+          <div className='field'>
+            <span className='label'>Password</span>
+            <input
+              className='input-h'
+              type='password'
+              placeholder='**********'
+            />
+          </div>
+
+          <div className='field row'>
+            <div className='checkbox-wrapper'>
+              <input type='checkbox' />
+              <span className='checkbox-label'>Remember me</span>
+            </div>
+
+            <span className='forgot-pass'>Forgot password</span>
+          </div>
+        </div>
+
+        <div className='buttons'>
+          <button className='primary input-h'>
+            <span>Sign in</span>
+          </button>
+          <button className='outlined input-h'>
+            <span>Sign in with Google</span>
+          </button>
+        </div>
+
+        <span className='text'>Don’t have an account? Sign up fo free!</span>
+      </div>
+    </Style>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,151 @@
+import styled from "styled-components";
+
+export const Style = styled.div`
+  .input-h {
+    height: 3.875rem;
+  }
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: "form background";
+
+  .background {
+    height: 100vh;
+    overflow: hidden;
+    img {
+      max-width: 100%;
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  .form {
+    grid-area: form;
+    display: grid;
+    grid-template-columns: 1fr max-content 1fr;
+    background: #f8f8f8;
+    padding: 8rem 0;
+
+    .buttons {
+      display: grid;
+      grid-template-rows: 1fr 1fr;
+      grid-column: 2/3;
+      margin-top: 14.5px;
+    }
+
+    button {
+      border: none;
+      border-radius: 15.3333px;
+      padding: 0;
+      font-size: 17.8889px;
+      line-height: 27px;
+    }
+
+    .primary {
+      color: #ffffff;
+      background: #ea454c;
+      box-shadow: 0px 5.11111px 12.7778px rgba(233, 68, 75, 0.25);
+    }
+
+    .outlined {
+      border: 1.27778px solid rgba(0, 0, 0, 0.25);
+      filter: drop-shadow(0px 5.11111px 12.7778px rgba(0, 0, 0, 0.1));
+      border-radius: 15.3333px;
+    }
+
+    > span {
+      grid-column: 2/3;
+    }
+
+    .header {
+      grid-column: 2/3;
+      .title {
+        font-size: 43.4444px;
+        line-height: 65px;
+        text-transform: uppercase;
+        color: #030303;
+      }
+
+      .subtitle {
+        font-weight: 400;
+        line-height: 27px;
+        color: #636364;
+      }
+    }
+
+    .fields {
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-row-gap: 1rem;
+      grid-column: 2/3;
+      margin-top: 48px;
+      input[type="password"],
+      input[type="email"] {
+        border: 1.27778px solid rgba(0, 0, 0, 0.25);
+        filter: drop-shadow(0px 5.11111px 12.7778px rgba(0, 0, 0, 0.25));
+        border-radius: 15.3333px;
+        padding: 0 18px;
+        font-size: 17.8889px;
+        line-height: 27px;
+        display: flex;
+        align-items: center;
+        margin-top: 8px;
+        background: none;
+      }
+
+      input[type="checkbox"] {
+        width: 18.48px;
+        height: 17.16px;
+        margin: 0;
+        border: 1.27778px solid rgba(0, 0, 0, 0.25);
+        border-radius: 5.11111px;
+        margin-right: 8px;
+        background: none;
+      }
+
+      .label {
+        line-height: 21px;
+        color: #181818;
+      }
+
+      .field {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin-top: 20px;
+
+        .checkbox-label {
+          font-size: 15.3333px;
+          line-height: 23px;
+          color: #181818;
+        }
+
+        .forgot-pass {
+          font-size: 15.3333px;
+          line-height: 23px;
+          color: #181818;
+        }
+
+        .checkbox-wrapper {
+          display: flex;
+          align-items: center;
+        }
+      }
+
+      .row {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+      }
+    }
+
+    .text {
+      font-size: 12.7778px;
+      line-height: 19px;
+      text-align: center;
+      letter-spacing: 0.03em;
+      color: #595959;
+      margin-top: 16px;
+    }
+  }
+`;
